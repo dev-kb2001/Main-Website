f46845cff204544301081d0ee255422525c6ac4f
MOHIT SAXENA <95961912+mohitsaxenaa@users.noreply.github.com>
2023-01-08 00:38:18 +0530

    WEBSITE 0.0.1
    please bas about us kardo jaldi se website done hai

---
+++ Main-Website-main/Main-Website-main/wesbite jmd/kunal.js
@@ -0,0 +1,40 @@
+
+    function reveal() {
+  var reveals = document.querySelectorAll(".xyz");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = ;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+    function reveal() {
+  var reveals = document.querySelectorAll(".imgpg3");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 0;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+
+
+
